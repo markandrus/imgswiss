ce3d91e78dccd5bd9929945d74dccf59d48f2eb9
Mark <markandrusroberts@gmail.com>
2011-03-10 00:01:19 -0600

    working

--- pipeline.rb
+++ pipeline.rb
@@ -1,85 +1,64 @@
 #!/usr/bin/env ruby
-# Parser for Image Filter Plans
+# pipeline.rb => Image Sequence Editor
 require 'rubygems'
 require 'RMagick'
 require 'ffmpeg'
 
 load 'options.rb'
+load 'utils.rb'
 load 'parser.rb'
 load 'transforms.rb'
 load 'classes.rb'
 
+# Parse options
 options = OptionParse.parse(ARGV)
 
-# Build our transformation pipeline
+# Parse any given plan files, accumulating in `$pipeline`
 $pipeline = Transforms.new
 options.plans.each do |plan|
-    if $verbose
-        $stderr.puts "Parsing plan: `" + plan + "'"
-    end
+    verbs "Parsing plan: `" + plan + "'"
     File.readlines(plan).each do |line|
-        line.chomp!
-        if $verbose
-            $stderr.puts '    "' + line + '"'
-        end
+        verbs '    "' + line.chomp! + '"'
         $pipeline.add(&parse(line))
     end
 end
 
-# Load images
-if options.stdin # are we processing stdin?
-    if $verbose
-        $stderr.puts "Reading single image from STDIN..."
-    end
-    file = Magick::Image.from_blob(ARGF.read)
-    if !file.nil?
-        if $verbose
-            $stderr.puts "    OK!"
-            $stderr.puts "Processing..."
-            $stderr.print "    STDIN"
-            options.plans.each { |plan| $stderr.print " >>= " + plan }
-            $stderr.puts " >>= STDOUT"
-        end
-        puts $pipeline.to_proc.call({"1" => file})['1'].to_blob
-    end
-elsif !options.indir.nil? # then let's load a dir of files
-    files = []
-    if $verbose
-        puts "Reading directory: `" + options.indir.path + "'"
-    end
-    options.indir.each do |file|
-        if file != '.' && file != '..'
-            if $verbose
-                $stderr.puts "    Adding `" + options.indir.path + file + "'"
-            end
-            files << Magick::ImageList.new(options.indir.path + file)
-        end
-    end
-    # Apply 
-    # NOTE: We could collapse this process with the one above
+def print_pipe(input, plans, output)
+    $stderr.print "    " + input
+    plans.each { |plan| $stderr.print " >>= " + plan }
+    $stderr.puts " >>= " + output
+end
+
+# STDIN
+if options.stdin
     if $verbose
         puts "Processing..."
+        print_pipe("STDIN", options.plans, "STDOUT")
     end
-    files.each do |file|
-        if $verbose
-            $stderr.print "    " + file.filename
-            options.plans.each { |plan| $stderr.print " >>= " + plan }
+	puts $pipeline.to_proc.call({"1" => Magick::Image.from_blob(ARGF.read)})['1'].to_blob
+
+# DIRECTORY
+elsif !options.indir.nil?
+    if $verbose
+        $stderr.puts "Reading directory: `" + options.indir.path + "'"
+        $stderr.puts "Processing..."
+    end
+    options.indir.each do |file|
+        # TODO: Replace this with an actual file-check
+        if file != '.' && file != '..' 
+            processed = $pipeline.to_proc.call({"1" => Magick::ImageList.new(options.indir.path + file)})['1']
             if !options.outdir.nil?
-                $stderr.puts " >>= " + options.outdir + file.filename.rpartition('/').last
+                processed.write(options.outdir + file)
+                if $verbose then print_pipe(file, options.plans, options.outdir + file)
             else
-                $stderr.puts " >>= " + file.filename
+                processed.write(options.indir.path + file)
+                if $verbose then print_pipe(file, options.plans, file)
             end
         end
-        processed = $pipeline.to_proc.call({"1" => file})['1'] # Process
-        if !options.outdir.nil? # write to outdir
-            processed.write(options.outdir + file.filename.rpartition('/').last)
-        end
-        if options.inplace # overwrite input file
-            processed.write(file.filename)
-        end
     end
-elsif !options.invideo.empty? # then let's read a video
-    frames = []
+
+# VIDEO
+elsif !options.invideo.empty?
     if $verbose
         puts "Reading video: `" + options.invideo + "'"
         #if !options.videostart.nil?
@@ -98,15 +77,15 @@ elsif !options.invideo.empty? # then let's read a video
     # TODO: Add a CLI option to specify initial offset
     # stream.seek(12)
     i = 0
-    # pts is presentation timestamp
-    # dts is decoding timestamp
+        # pts is presentation timestamp
+        # dts is decoding timestamp
     video.first_video_stream.decode_frame do |frame, pts, dts|
         i += 1
         # TODO: Add a CLI option to specify the end
-        # stop when decoding timestamp (~position) reach 18
-        # break if dts > 18
-        # TODO: Change the 5 in the following. I don't know what this is? FPS?
-        # decode 1 frame for 5
+            # stop when decoding timestamp (~position) reach 18
+            # break if dts > 18
+            # TODO: Change the 5 in the following. I don't know what this is? FPS?
+            # decode 1 frame for 5
         next unless i % 5 == 0
         # TODO: Add a CLI option to specify output frame filetype
         if !options.outdir.nil?
